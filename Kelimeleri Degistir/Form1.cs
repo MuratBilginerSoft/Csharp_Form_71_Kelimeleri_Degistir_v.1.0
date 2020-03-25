203c26d44ba741cc9f7c0601f95d615a136c6a60
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:06:33 +0300

    Initial commit

---
+++ Kelimeleri Degistir/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kelimeleri_Degistir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string metin = textBox1.Text;
+
+            string aranan = textBox2.Text;
+
+            string değişecek = textBox3.Text;
+
+            bool x = metin.Contains(aranan);
+
+            if (x)
+            {
+                metin = metin.Replace(aranan, değişecek);
+                textBox1.Text = metin;
+            }
+        }
+    }
+}
